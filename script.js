95c711f42c05e04a344a787fc3875f871fb2d42f
Dan Digby <dandigby@me.com>
2021-05-19 20:17:19 +0100

    added starter javascript

---
+++ script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form_input--type');
+const inputDistance = document.querySelector('.form_input--distance');
+const inputDuration = document.querySelector('.form_input--duration');
+const inputCadence = document.querySelector('.form_input--cadence');
+const inputElevation = document.querySelector('.form_input--elevation');
+
