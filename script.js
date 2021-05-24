ac3b14f811f92af3dd77966c5452a1633d749591
Dan Digby <dandigby@me.com>
2021-05-24 17:55:27 +0100

    move to marker on click function

--- script.js
+++ script.js
@@ -6,6 +6,8 @@
 class Workout{
   date = new Date();
   id = (new String(Date.now()).slice(-10));
+  clicks = 0;
+
   constructor(coords, distance, duration){
     this.coords = coords; // [lat, long]
     this.distance = distance; //in km
@@ -15,6 +17,10 @@ class Workout{
   _setDescription(){
     const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
     this.description = `${this.type[0].toUpperCase()}${this.type.slice(1)} on ${months[this.date.getMonth()]} ${this.date.getDate()}`
+  }
+
+  click(){
+    this.clicks++;
   }
 }
 
@@ -61,12 +67,14 @@ const inputElevation = document.querySelector('.form_input--elevation');
 // Application Architecture
 class App {
   #map;
+  #mapZoomLevel = 13;
   #mapEvent;
   #workouts = [];
   constructor(){
     this._getPosition();
     form.addEventListener('submit', this._newWorkout.bind(this));
-    inputType.addEventListener('change', this._toggleElevationField)
+    inputType.addEventListener('change', this._toggleElevationField);
+    containerWorkouts.addEventListener('click', this._moveToPopup.bind(this));
   }
   _getPosition(){
     if(navigator.geolocation)
@@ -83,7 +91,7 @@ navigator.geolocation.getCurrentPosition(this._loadMap.bind(this), function(){
     
       const coords = [latitude, longitude];
     
-      this.#map = L.map('map').setView(coords, 13);
+      this.#map = L.map('map').setView(coords, this.#mapZoomLevel);
     
     L.tileLayer('https://{s}.tile.openstreetmap.fr/hot//{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -157,8 +165,7 @@ navigator.geolocation.getCurrentPosition(this._loadMap.bind(this), function(){
 
     // Add new object to workout array
     this.#workouts.push(workout);
-    console.log(workout)
-
+  
     // Render workout as marker on map
     this._renderWorkoutMarker(workout);
   
@@ -229,6 +236,26 @@ navigator.geolocation.getCurrentPosition(this._loadMap.bind(this), function(){
       `
 
       form.insertAdjacentHTML('afterend', html);
+  }
+
+  _moveToPopup(e){
+    const workoutEL = e.target.closest('.workout');
+    if(!workoutEL) return
+    console.log(workoutEL)
+
+
+    const workout = this.#workouts.find(work => work.id === workoutEL.dataset.id);
+    console.log(workout)
+
+    this.#map.setView(workout.coords, this.#mapZoomLevel, {
+      animate: true,
+      pan: {
+        duration: 1,
+      }
+    });
+
+    // using the public interface
+    workout.click();
   }
 }
 
