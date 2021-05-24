035a512b003676c6abbad78402ef16547a518749
Dan Digby <dandigby@me.com>
2021-05-24 11:29:31 +0100

    refactored code into new class

--- script.js
+++ script.js
@@ -10,66 +10,76 @@ const inputDuration = document.querySelector('.form_input--duration');
 const inputCadence = document.querySelector('.form_input--cadence');
 const inputElevation = document.querySelector('.form_input--elevation');
 
-let map, mapEvent;
 
-if(navigator.geolocation)
-navigator.geolocation.getCurrentPosition(function(position){
-  const {latitude} = position.coords;
-  const {longitude} = position.coords;
-  console.log(`https://www.google.co.uk/maps/@${latitude},${longitude}`)
 
-  const coords = [latitude, longitude];
+class App {
+  #map;
+  #mapEvent;
+  constructor(){
+    this._getPosition();
 
-  map = L.map('map').setView(coords, 13);
-
-L.tileLayer('https://{s}.tile.openstreetmap.fr/hot//{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
-
-  //handling clicks on map
-    map.on('click', function(mapE){
-      mapEvent = mapE;
-      form.classList.remove('hidden');
-      inputDistance.focus();
-      
-      // console.log(mapEvent)
-    //   const {lat, lng} = mapEvent.latlng;
-    //   L.marker([lat,lng]).addTo(map)
-    // .bindPopup(L.popup({
-    //   maxWidth: 250,
-    //   minWidth: 100,
-    //   autoClose: false,
-    //   closeOnClick: false,
-    //   className: 'running-popup',
-    // }))
-    // .setPopupContent('Workout')
-    // .openPopup();
-    })
-}, 
-function(){
+    form.addEventListener('submit', this._newWorkout.bind(this));
+    
+    inputType.addEventListener('change', this._toggleElevationField)
+  }
+  _getPosition(){
+    if(navigator.geolocation)
+navigator.geolocation.getCurrentPosition(this._loadMap.bind(this), function(){
   alert('Could not get your position...');
 });
+  }
 
-form.addEventListener('submit', function(e){
-  inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = '';
-  // Display marker
-    console.log(mapEvent)
-      const {lat, lng} = mapEvent.latlng;
-      L.marker([lat,lng]).addTo(map)
-    .bindPopup(L.popup({
-      maxWidth: 250,
-      minWidth: 100,
-      autoClose: false,
-      closeOnClick: false,
-      className: 'running-popup',
-    }))
-    .setPopupContent('Workout')
-    .openPopup();
-    e.preventDefault();
-})
+  _loadMap(position){
+    
+      const {latitude} = position.coords;
+      const {longitude} = position.coords;
+      console.log(`https://www.google.co.uk/maps/@${latitude},${longitude}`)
+    
+      const coords = [latitude, longitude];
+    
+      this.#map = L.map('map').setView(coords, 13);
+    
+    L.tileLayer('https://{s}.tile.openstreetmap.fr/hot//{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(this.#map);
+    
+      //handling clicks on map
+        this.#map.on('click', this._showForm.bind(this))
+     
+  }
 
-inputType.addEventListener('change', function(){
-  inputElevation.closest('.form_row').classList.toggle('form_row--hidden')
-  inputCadence.closest('.form_row').classList.toggle('form_row--hidden')
-})
+  _showForm(mapE){
+    this.#mapEvent = mapE;
+          form.classList.remove('hidden');
+          inputDistance.focus();
+  }
+
+  _toggleElevationField(){
+    inputElevation.closest('.form_row').classList.toggle('form_row--hidden')
+      inputCadence.closest('.form_row').classList.toggle('form_row--hidden')
+  }
+
+  _newWorkout(e){
+    inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = '';
+    // Display marker
+      console.log(this.#mapEvent)
+        const {lat, lng} = this.#mapEvent.latlng;
+        L.marker([lat,lng]).addTo(this.#map)
+      .bindPopup(L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup',
+      }))
+      .setPopupContent('Workout')
+      .openPopup();
+      e.preventDefault();
+  }
+}
+
+const app = new App();
+
+
+
 
